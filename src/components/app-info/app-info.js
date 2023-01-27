3ee20aa500da91513993315a1acb60de3d076796
Orest Dacko <orestdacko@gmail.com>
2023-01-27 16:54:02 +0200

    changed title

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -2,7 +2,7 @@ import "./app-info.css";
 const AppInfo = ({increased,employees}) => {
   return (
     <div className="app-info">
-      <h1>Звіт працівників в компанії FriendsCompany:</h1>
+      <h1>Звіт працівників в компанії FriendsCompany2023:</h1>
       <h2>Загальна кількість працівників:{employees}</h2>
       <h2>Премію отримають:{increased}</h2>
     </div>
